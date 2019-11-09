27c12488d4b6bf76686ef13970447540f36fd649
hutianyu2006 <52768188+hutianyu2006@users.noreply.github.com>
2019-11-09 20:03:00 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,8 +3,8 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDDgYM74S47gs8QfABRdKY8SgTWnWMXtQ8');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'My private conversation');
-define('TITLENAME', 'OK');
+define('SITE_NAME', 'Youtube Mirror for MX Tecnology studio');
+define('TITLENAME', 'Youtube Mirror for MX Tecnology studio');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
