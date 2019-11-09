0c21612aa3f713aedae49ef9e0f899f981db1324
hutianyu2006 <52768188+hutianyu2006@users.noreply.github.com>
2019-11-09 19:44:43 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyDDgYM74S47gs8QfABRdKY8SgTWnWMXtQ8');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'Hello World!');
-define('TITLENAME', 'Hello World!');
+define('SITE_NAME', 'My private conversation');
+define('TITLENAME', 'OK');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
